97dc3c108adca5e5461c43cf46f05a4e0d190587
James Ratcliff <james.ratcliff@offers.com>
2021-04-20 17:55:56 -0500

    Working version

--- README.md
+++ README.md
@@ -1,8 +1,206 @@
-# node-express-mysql-simple-api
-A Sample API to demonstrate creating APIs using NodeJs, Express.js and MySQL.
+# Sample Stock App
 
-https://referbruv.com/blog/posts/developing-a-simple-api-with-nodejs-express-and-mysql
+A sample node.js simple server to calculate gains and losses for a stock portfolio. 
 
-Learn how to deploy this API into AWS Lambda and link to API Gateway for access by means of aws-serverless-express and cloudformation.
 
-https://referbruv.com/blog/posts/deploying-expressjs-api-to-aws-lambda-and-api-gateway
+
+Requirements: (rewritten)
+
+Goal: The project is a microservice to determine the total profit or loss of a user’s 
+stock portfolio performance between 2 timestamps.
+
+
+-- Tools Used: Node.js plus various modules, Express, mysql (not needed), Got for external api calls. 
+
+-- The project is fairly straightforward, though I feel the requirements could be rewritten a little 
+bit to be clearer. 
+
+
+There are 2 components to this project:
+1) System architecture. Describe how you would design the service(s) and how it would
+be integrated with an existing system.
+
+  --A basic REST service was created, mainly giving access to a /api/gains endpoint. 
+The endpoint allows for a username, and two optional dates to be used.
+
+http://localhost:3000/api/gains/tim_apple
+
+http://localhost:3000/api/gains/tim_apple_senior?fromDate=2-2-2021&toDate=04-01-2021
+
+A singular file was used for this small example, so all methods are in app.js, would 
+normally be broken up into needed areas. 
+No real modeling was needed for db part, but I included my work on a Users object, I am 
+using for a series of tutorials.
+ 
+Prep work files was done for AWS lambda deployment.   
+
+a) You may assume that a service already exists that can provide data for all of the
+stock transactions (buy or sell, number of shares, price per share, timestamp) for
+all users.
+
+  -- A mock method getMockStockTradesAPI(user, fromDate, toDate) is created and filled with 
+  data for 6 or so users keyed on username. 
+  
+b) Your service may not access the data store containing the transaction data
+directly. It’s up to you to decide how you’d like your service to get access to this
+data.
+
+  -- mocked in method above, could be replaced with an API service call. 
+  
+c) Don’t reinvent the wheel. We want you to leverage cloud services (AWS) as
+much as possible.
+
+  -- Due to my time constraints, I did not fully deploy it as a lambda service. 
+  
+d) Be specific. Visual architecture diagrams are nice, but we want to see your vision
+for exactly how each piece of the system will work.
+
+
+2) Write code. We don’t expect you to implement the entire service end-to-end, but we
+want you to build the significant pieces.
+
+a) Integrate a 3rd party API (such as https://finnhub.io/docs/api/quote) to get stock
+price data.
+  
+  -- Current prices were gathered from the 3rd party finnhub to use in calculations.  
+  This was NOT optimized to prevent multiple calls for data already in hand, but not
+  necessary for this smaller project. 
+  
+  A multi quote call could also be used to speed up external requests. 
+  
+b) Mock up the transaction history for a few pretend, but realistic users. See below
+for an example.
+  
+  -- Mock data in the getMockStockTradesAPI method, 
+  JSON format as a text block which we parsed. 
+```
+      data['john_simple'] =
+  `12345, buy, GME, 5, 20.99, 2020-12-21 15:45:24
+  12345, sell, GME, 5, 145.04, 2021-01-26 18:34:12`;
+```
+
+c) Calculate the profit/loss of the portfolio from the above data.
+
+-- This part was a bit vague as to expected returns, simplest version of this could be for a 
+single user, a single float value representing total gains and losses over the period, or
+the value for all users. 
+
+I choose a simple and a more complex return value. 
+
+Example output:
+
+The simple return shows the date period, and a totalGainLoss value. 
+
+The longer return shows each full trade, from buy to sell, with price and quantity, 
+and gainLoss of that sale. 
+
+```
+Long:
+{
+  "user": "tim_apple_senior",
+  "fromDate": "2-2-2021",
+  "toDate": "04-01-2021",
+  "totalGainLoss": "1024.14",
+  "trades": [
+    {
+      "userId": "12345",
+      "action": "buy",
+      "symbol": "GME",
+      "quantity": 5,
+      "price": "20.99",
+      "tradeTime": "2020-12-21 15:45:24",
+      "quantitySell": 5,
+      "priceSell": "145.04",
+      "tradeTimeSell": "2021-01-26 18:34:12",
+      "gainLoss": "620.25"
+    },
+    {
+      "userId": "12345",
+      "action": "buy",
+      "symbol": "AAPL",
+      "quantity": 5,
+      "price": "76.60",
+      "tradeTime": "2020-01-02 16:01:23",
+      "quantitySell": 5,
+      "priceSell": "90.00",
+      "tradeTimeSell": "2020-06-08 12:12:51",
+      "gainLoss": "67.00"
+    },
+    {
+      "userId": "12345",
+      "action": "buy",
+      "symbol": "AAPL",
+      "quantity": 1,
+      "price": "95.11",
+      "tradeTime": "2020-06-05 15:21:35",
+      "quantitySell": 1,
+      "priceSell": "90.00",
+      "tradeTimeSell": "2020-06-08 12:12:51",
+      "gainLoss": "-5.11"
+    },
+    {
+      "userId": "12345",
+      "action": "buy",
+      "symbol": "AAPL",
+      "quantity": 9,
+      "price": "95.11",
+      "tradeTime": "2020-06-05 15:21:35",
+      "quantitySell": 9,
+      "priceSell": 133.11,
+      "tradeTimeSell": "2021-04-20 21:56:46",
+      "gainLoss": "342.00"
+    }
+  ]
+}
+```
+
+d) Integrate the cloud services used by your architecture.
+  
+  -- Time limit for myself did not allow for lamda full creation. 
+  
+e) Create the interface that will allow other parts of the system to access the data
+calculated by your service.
+
+  -- Api can be called externally 
+  
+  http://localhost:3000/api/gains/tim_apple
+  
+  And Json results parsed for front end display. 
+
+
+
+I have created an maintained a personal simple stock market application that also shows 
+grouping by industry of currently held stocks,  adding and removing stocks by ticker search, 
+opening and closing prices, current price, daily and term gains, I have used this for over 
+5 years now to help control my portfolio.  
+
+
+Notes on project creation:
+
+  The main thing to watch for in a project like this is that buy and sale quantity amounts may not 
+match up exactly, and each case needs to be taken into account. 
+
+  Simple Example 1: Buy 5 GME stocks and sell 5 GME stocks
+
+  Ex 2: Buy multiple times, sell once. 
+
+  Ex 3: Buy once, sell multiple times. 
+
+  Ex 4: Buy once, never sell (use current stock price then)
+
+  Buy x times Sell y times. 
+
+  There is a case as well where there could be no buys during the date period, only Sells. 
+
+
+  In this case there is no gain or loss calculated, as the initial price is considered unknown. 
+
+Little error checking or data integrity is done in the sample project. 
+
+A small debug section is left at the bottom of app.js to directly check several methods. 
+
+
+Hours Used: 4.5 
+
+James Ratcliff
+April 20, 2021.
--- app.js
+++ app.js
@@ -1,19 +1,262 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const mysql = require('mysql');
 const port = 3000;
+const got = require('got');
+
 
 const config = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
+    database: process.env.DB_NAME,
 };
+
 const pool = mysql.createPool(config);
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get("/api/gains/:username", (req, res) => {
+
+    console.log(' query = ', req.query);
+    console.log(' query fromDate = ', req.query.fromDate);
+
+    let fromDate = req.query.fromDate;
+    let toDate = req.query.toDate;  // NOTE: no error handling, defaults in method.
+
+    // calculateStockResults(req.params['username'], '2021-01-01', '2021-05-01', 1)
+    calculateStockResults(req.params['username'], fromDate, toDate, 1)
+        .then(function (results) {
+            // handle data
+            res.setHeader('Content-Type', 'application/json');
+            res.send(results);
+        });
+
+});
+
+
+// Given a user and a start date, go request all their trades, and calculate their gains/losses.
+// For current stock prices, call over to finnhub.io
+// Note: this will not add in any values for sells where the matching buy is before the time period.
+// A long and short version of this report are available.
+// longresults flag will include buy.sell trades.
+async function calculateStockResults(user, fromDate, toDate, longResults) {
+    let currDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    if (!fromDate) fromDate = '2021-01-01 01:00:00';
+    if (!toDate) toDate = currDate;
+
+    let data = getMockStockTradesAPI(user, fromDate, toDate);
+    // console.log('DEBUG data = ', data);
+    let trades = parseCSVData(data);
+
+    // console.log('DEBUG parsed trades = ', trades);
+
+    // Step 1:
+    // Get a total of all stock quantities bought.
+    let t = 0;
+    let stockQuantities = {};
+    while (trades[t]) {
+        if (trades[t].action === "buy") {
+            if (!stockQuantities[trades[t].symbol]) stockQuantities[trades[t].symbol] = 0;
+            stockQuantities[trades[t].symbol] += trades[t].quantity;
+        }
+        t++;
+    }
+    // console.log('DEBUG stockQuantities = ', stockQuantities);
+
+
+    // Step 2:
+    // Loop over each trade, get buy and sell dates and amounts if known.
+    let totalGainLoss = 0;
+    let fullTrades = [];
+    t = 0;
+    while (trades[t]) {
+
+        let sellTrade = trades[t];
+        if (sellTrade.action === "sell") {
+            // Match to a buy if possible.
+            // Note could be multiples... purchases for a single sell. and mult sells.
+            let u = 0;
+            while (trades[u] && u < t && stockQuantities[sellTrade.symbol] > 0) {
+                if (trades[u].symbol === sellTrade.symbol && trades[u].action === "buy") {
+                    let fullTrade = {...trades[u]};  // clone our object.
+                    let maxQuantity = Math.min(sellTrade.quantity, trades[u].quantity);  // dont sell or buy more than we have.  hmmm right?
+
+                    fullTrade.quantity = maxQuantity;
+                    fullTrade.quantitySell = maxQuantity;
+                    fullTrade.priceSell = sellTrade.price;
+                    fullTrade.tradeTimeSell = sellTrade.tradeTime;
+
+                    stockQuantities[sellTrade.symbol] -= maxQuantity;  // update our index.
+
+                    trades[t].quantity -= maxQuantity;  // update our sells.
+                    trades[u].quantity -= maxQuantity;  // subtract out our old buy trade one.
+
+                    // Calc our gain loss for this set.
+                    fullTrade.gainLoss = (fullTrade.quantity * (fullTrade.priceSell - fullTrade.price)).toFixed(2);
+                    totalGainLoss += parseFloat(fullTrade.gainLoss);
+                    // console.log('totalGainLoss = ', totalGainLoss);
+
+                    fullTrades.push(fullTrade)
+                }
+                u++;
+            }
+        }
+        t++;
+    }
+    // console.log('fullTrades = ', fullTrades);
+
+
+    // Step 3: Fill in any final current amounts on trades.
+    t = 0;
+    while (trades[t]) {
+        if (trades[t].action === "buy" && trades[t].quantity > 0) {
+            let fullTrade = {...trades[t]};  // clone our object.
+
+            fullTrade.quantitySell = fullTrade.quantity;
+
+            // External api call to get current price.
+            let currPrice = await getCurrentStockPrice(fullTrade.symbol);
+            fullTrade.priceSell = currPrice;
+            fullTrade.tradeTimeSell = currDate;
+
+            stockQuantities[fullTrade.symbol] -= fullTrade.quantity;  // update our index.
+
+            trades[t].quantity -= fullTrade.quantity;  // subtract out our old buy trade one.
+
+            // Calc our gain loss for this set.
+            fullTrade.gainLoss = (fullTrade.quantity * (fullTrade.priceSell - fullTrade.price)).toFixed(2);
+            totalGainLoss += parseFloat(fullTrade.gainLoss);
+
+            fullTrades.push(fullTrade)
+        }
+
+        t++;
+    }
+
+    console.log('fullTrades = ', fullTrades);
+
+    // Full report.
+    let output = {
+        user: user,
+        fromDate: fromDate,
+        toDate: toDate,
+        totalGainLoss: totalGainLoss.toFixed(2),
+    };
+
+    // If we want full data include this flag
+    if (longResults) output.trades = fullTrades;
+
+    return JSON.stringify(output);
+}
+
+// Asks for api current price from stock service.
+// No real error catching here.
+async function getCurrentStockPrice(symbol) {
+    let apiToken = "c1vjtst37jkpmefgjjvg"; // Hard coded
+    let url = "https://finnhub.io/api/v1/quote?symbol=" + symbol + "&token=" + apiToken;
+
+    try {
+        const response = await got(url);
+        // console.log('DEBUG api response = ', response.body);
+        let json = JSON.parse(response.body);
+        // console.log('response.body c = ', json.c);
+
+        return json.c;
+    } catch (error) {
+        console.log(error.response.body);
+    }
+
+    return null;
+}
+
+
+function parseCSVData(data) {
+    if (!data) return [];
+
+    let lines = data.split(/\n/);
+
+    // console.log('DEBUG lines = ', lines);
+
+    let trades = [];
+    lines.forEach(function (line) {
+        // console.log('DEBUG line = ', line);
+        let [userId, action, symbol, quantity, price, tradeTime] = line.split(/, */);
+
+        let trade = {
+            userId: userId,
+            action: action,
+            symbol: symbol,
+            quantity: quantity,
+            price: price,
+            tradeTime: tradeTime
+        };
+        trades.push(trade);
+    });
+
+    return trades;
+}
+
+// Grab all trades for this user and time period.
+// Assumption: trades are in date order correctly.
+// Can use userid instead of a username.
+function getMockStockTradesAPI(user, fromDate, toDate) {
+    // user_id, buy/sell, stock symbol, quantity, price, timestamp (UTC)
+    // 12345, buy, AAPL, 10, 76.60, 2020-01-02 16:01:23
+    // 12345, buy, AAPL, 5, 95.11, 2020-06-05 15:21:65
+    // 12345, buy, GME, 5, 20.99, 2020-12-21 15:45:24
+    // 12345, sell, GME, 5, 145.04, 2021-01-26 18:34:12
+
+    // MOCK USERS, would come from another external api later.
+
+    let data = [];
+
+    // Simple example buy sell exact same amount.
+    data['john_simple'] =
+        `12345, buy, GME, 5, 20.99, 2020-12-21 15:45:24
+12345, sell, GME, 5, 145.04, 2021-01-26 18:34:12`;
+
+    // one sell spans over two buys.
+    data['tim_apple'] =
+        `12345, buy, AAPL, 5, 76.60, 2020-01-02 16:01:23
+12345, buy, AAPL, 10, 95.11, 2020-06-05 15:21:35
+12345, sell, AAPL, 6, 90.00, 2020-06-08 12:12:51`;
+
+    // Example, sells way more than he really owns.
+    data['peter_sellers'] =
+        `12345, buy, AAPL, 5, 76.60, 2020-01-02 16:01:23
+12345, sell, AAPL, 10, 95.11, 2020-06-05 15:21:35
+12345, sell, AAPL, 6, 90.00, 2020-06-08 12:12:51`;
+
+    // Example, sells only in range.
+    data['jackie_cellars'] =
+        `12345, sell, AAPL, 10, 95.11, 2020-06-05 15:21:35
+12345, sell, AAPL, 6, 90.00, 2020-06-08 12:12:51`;
+
+    // Example, single buy action only.
+    data['johnny_single'] =
+        `12345, buy, AAPL, 5, 95.11, 2020-06-05 15:21:35`;
+
+    // Example, double buy action.
+    data['johnny_double'] =
+        `12345, buy, AAPL, 5, 95.11, 2020-06-05 15:21:35
+12345, buy, AAPL, 5, 90.00, 2020-06-08 12:12:51`;
+
+    // one sell spans over two buys plus a second stock
+    data['tim_apple_senior'] =
+        `12345, buy, GME, 5, 20.99, 2020-12-21 15:45:24
+12345, sell, GME, 5, 145.04, 2021-01-26 18:34:12
+12345, buy, AAPL, 5, 76.60, 2020-01-02 16:01:23
+12345, buy, AAPL, 10, 95.11, 2020-06-05 15:21:35
+12345, sell, AAPL, 6, 90.00, 2020-06-08 12:12:51`;
+
+    // If no user known, return empty [] and handles fine.
+    return data[user];
+}
+
 
 app.get("/api/users", (req, res) => {
     pool.getConnection((err, conn) => {
@@ -29,7 +272,7 @@ app.get("/api/users", (req, res) => {
 app.post("/api/users", (req, res) => {
     let body = req.body;
     let user = {
-        "name": body["name"],
+        "fullname": body["fullname"],
         "email": body["email"]
     };
     pool.getConnection((err, conn) => {
@@ -45,9 +288,9 @@ app.post("/api/users", (req, res) => {
 app.patch("/api/users/:user_id", (req, res) => {
     let body = req.body;
     let userid = req.params["user_id"];
-    let name = body["name"];
+    let fullname = body["fullname"];
     let email = body["email"];
-    let query = `UPDATE users SET name='${name}',email='${email}' WHERE id=${userid}`;
+    let query = `UPDATE users SET fullname='${fullname}',email='${email}' WHERE id=${userid}`;
     pool.getConnection((err, conn) => {
         if (err) throw err;
         conn.query(query, (error, results, fields) => {
@@ -71,6 +314,24 @@ app.delete("/api/users/:user_id", (req, res) => {
     })
 });
 
-// app.listen(port, () => {
-//     console.log("server started to listen on " + port);
-// });
+// For local development.
+app.listen(port, () => {
+    console.log("server started to listen on " + port);
+});
+
+
+// DEBUG TEST
+// calculateStockResults('tim_apple_senior', '2021-01-01', '2021-05-01', true)
+//     .then(function (results) {
+//         console.log('results = ', results);
+//     });
+
+
+// let price = getCurrentStockPrice("AAPL")
+// console.log('price = ', price);
+// let price2 = getCurrentStockPrice("GOOG")
+// console.log('price2 = ', price2);
+
+
+
+
